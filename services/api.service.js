a4978489f6c4bb29a53d74031701464e766d6074
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-09-27 00:18:01 +0100

    tried making them look thesame

--- services/api.service.js
+++ services/api.service.js
@@ -23,10 +23,31 @@ module.exports = {
 
 		// Global Express middlewares. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Middlewares
 		use: [],
+		// Global CORS settings for all routes
+		cors: {
+			// Configures the Access-Control-Allow-Origin CORS header.
+			origin: "*",
+			// Configures the Access-Control-Allow-Methods CORS header.
+			methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+			// Configures the Access-Control-Allow-Headers CORS header.
+			allowedHeaders: "*",
+			// Configures the Access-Control-Expose-Headers CORS header.
+			exposedHeaders: "*",
+			// Configures the Access-Control-Allow-Credentials CORS header.
+			credentials: false,
+			// Configures the Access-Control-Max-Age CORS header.
+			maxAge: 7200
+		},
 
 		routes: [
 			{
 				path: "/api",
+
+				cors: {
+					origin: ["http://localhost:3000", "https://localhost:4000"],
+					methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+					credentials: true
+				},
 
 				whitelist: [
 					"**"
